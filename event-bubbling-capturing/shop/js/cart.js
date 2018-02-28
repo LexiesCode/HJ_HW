e2dd376ac739fbf9e0273065e5f0e0c0d3107caa
LexiesCode <35810389+LexiesCode@users.noreply.github.com>
2018-02-28 17:20:25 +0300

    Выполнены задания

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const products = document.querySelector('.items-list');
+
+function addItemToCart(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains('add-to-cart')) {
+        return;
+    }
+    addToCart({
+        title: event.target.dataset.title,
+        price: event.target.dataset.price
+    });
+}
+
+products.addEventListener('click', addItemToCart);
--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,22 @@
 'use strict';
 
+const header = document.getElementsByTagName('th');
+
 function handleTableClick(event) {
-  
+    if (event.target.classList.contains('prop__name')) {
+        switch (event.target.dataset.dir) {
+            case '-1':
+                event.target.dataset.dir = 1;
+                break;
+            case '1':
+                event.target.dataset.dir = -1;
+                break;
+            default:
+                event.target.dataset.dir = 1;
+        }
+        event.currentTarget.dataset.sortBy = event.target.dataset.propName;
+        sortTable(event.currentTarget.dataset.sortBy, event.target.dataset.dir);
+    }
 }
+
+header.addEventListener('click', handleTableClick);
