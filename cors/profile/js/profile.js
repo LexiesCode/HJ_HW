e42630ca4e88eed07a90ad2a1cf5fc2a2f3ca78d
LexiesCode <35810389+LexiesCode@users.noreply.github.com>
2018-03-11 02:34:48 +0300

    Выполнены задания 1, 2

--- cors/profile/js/profile.js
+++ cors/profile/js/profile.js
@@ -1,1 +1,53 @@
-'use strict';
+﻿'use strict';
+
+const userName = document.querySelector('[data-name]');
+const description = document.querySelector('[data-description]');
+const pic = document.querySelector('[data-pic]');
+const position = document.querySelector('[data-position]');
+const technologies = document.querySelector('[data-technologies]');
+const content = document.querySelector('.content');
+
+function getRandomName() {
+    return 'callback' + Math.round(Math.random() * 1000000);
+}
+
+function loadData(url) {
+    const callbackFunc = getRandomName();
+    return new Promise((done, fail) => {
+        window[callbackFunc] = done;
+
+        const script = document.createElement('script');
+        script.src = `${url}?jsonp=${callbackFunc}`;
+        document.body.appendChild(script);
+    });
+}
+
+function showData(data) {
+    userName.textContent = data.name;
+    description.textContent = data.description;
+    pic.src = data.pic;
+    position.textContent = data.position;
+    return new Promise((done, fail) => {
+        done(data.id);
+    });
+}
+
+function showTech(techs) {
+    technologies.innerHTML = '';
+    for (let techItem of techs) {
+        addTech(techItem);
+    }
+
+    function addTech(tech) {
+        const techIcon = document.createElement('span');
+        techIcon.classList.add('devicons', `devicons-${tech}`);
+        technologies.appendChild(techIcon);
+    }
+    content.style.display = 'initial';
+}
+
+
+loadData('https://neto-api.herokuapp.com/profile/me')
+    .then(showData)
+    .then(id => loadData(`https://neto-api.herokuapp.com/profile/${id}/technologies`))
+    .then(showTech);
--- cors/twitter/js/twitter.js
+++ cors/twitter/js/twitter.js
@@ -1,1 +1,36 @@
-'use strict';
+﻿'use strict';
+const background = document.querySelector('.bio img');
+const userName = document.querySelector('.desc h3');
+const description = document.querySelector('.desc p');
+const pic = document.querySelector('.avatarcontainer img');
+const tweets = document.querySelector('output[data-tweets]');
+const followers = document.querySelector('output[data-followers]');
+const following = document.querySelector('output[data-following]');
+
+function getRandomName() {
+    return 'callback' + Math.round(Math.random() * 1000000);
+}
+
+function loadData(url) {
+    const callbackFunc = getRandomName();
+    return new Promise((done, fail) => {
+        window[callbackFunc] = done;
+
+        const script = document.createElement('script');
+        script.src = `${url}?jsonp=${callbackFunc}`;
+        document.body.appendChild(script);
+    });
+}
+
+function showWidget(widget) {
+    background.src = widget.wallpaper;
+    userName.textContent = widget.username;
+    description.textContent = widget.description;
+    pic.src = widget.pic;
+    tweets.textContent = widget.tweets;
+    followers.textContent = widget.followers;
+    following.textContent = widget.following;
+}
+
+loadData('https://neto-api.herokuapp.com/twitter/jsonp')
+    .then(showWidget);
